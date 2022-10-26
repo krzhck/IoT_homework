82f0f7d10bbe95daa4bdaee5b093ae3864a64526
krzhck <isaaxou@icloud.com>
2022-10-26 19:57:20 +0800

    init pa4

---
+++ pa4/readme.txt
@@ -0,0 +1,5 @@
+cd src
+pip3 install -r requirements.txt
+
+# 运行
+python3 main.py
---
+++ pa4/src/main.py
@@ -0,0 +1,6 @@
+import wave
+import matplotlib.pyplot as plt
+import numpy as np
+from math import *
+from scipy import signal
+
